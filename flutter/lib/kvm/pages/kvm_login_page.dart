e4d28d9e912526a72d2fb8f67cf1ba88cc8a3a89
Teo Vogel <teovogel97@gmail.com>
2024-10-15 16:30:07 -0300

    New LoginPage

--- flutter/lib/kvm/pages/kvm_login_page.dart
+++ flutter/lib/kvm/pages/kvm_login_page.dart
@@ -37,116 +37,139 @@ class _KVMLoginPageState extends State<KVMLoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("KVM Login"),
+        backgroundColor: Colors.transparent,
+        foregroundColor: Theme.of(context).colorScheme.onBackground,
+        elevation: 0,
       ),
-      body: Center(
-        child: Card(
-          margin: EdgeInsets.all(16),
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  TextFormField(
-                    controller: usernameController,
-                    decoration: InputDecoration(
-                      labelText: "User",
-                      border: const UnderlineInputBorder(),
-                    ),
-                    validator: (value) {
-                      if (value ==
-                              null /* ||
-                          IRemoteAuth.validateEmailAddress(value.trim())*/
-                          ) {
-                        return "error";
-                      }
-                      return null;
-                    },
-                  ),
-                  Column(
-                    children: [
-                      const SizedBox(height: 16.0),
+      body: LayoutBuilder(builder: (context, constraints) {
+        return SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minWidth: constraints.maxWidth,
+              minHeight: constraints.maxHeight,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Form(
+                key: _formKey,
+                child: IntrinsicHeight(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Text(
+                        "Iniciar Sesión",
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineSmall
+                            ?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 16),
                       TextFormField(
-                        obscureText: !passwordVisible,
-                        controller: passwordController,
+                        controller: usernameController,
+                        decoration: InputDecoration(
+                          labelText: "Usuario",
+                          border: const OutlineInputBorder(),
+                        ),
                         validator: (value) {
-                          if (value == null || value.trim().isEmpty) {
+                          if (value ==
+                                  null /* ||
+                                IRemoteAuth.validateEmailAddress(value.trim())*/
+                              ) {
                             return "error";
                           }
                           return null;
                         },
-                        decoration: InputDecoration(
-                            labelText: "Password",
-                            border: const UnderlineInputBorder(),
-                            suffixIcon: IconButton(
-                              icon: Icon(
-                                passwordVisible
-                                    ? Icons.visibility_off
-                                    : Icons.visibility,
-                              ),
-                              onPressed: () {
-                                setState(() {
-                                  passwordVisible = !passwordVisible;
-                                });
-                              },
-                            )),
                       ),
-                    ],
-                  ),
-                  const SizedBox(height: 32),
-                  FilledButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        _loginPressed();
-                      }
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text("Log in"),
-                        if (isLogingIn)
-                          Padding(
-                            padding: const EdgeInsets.only(left: 8),
-                            child: SizedBox.square(
-                                dimension: 16,
-                                child: CircularProgressIndicator(
-                                  color: Colors.white,
+                      Column(
+                        children: [
+                          const SizedBox(height: 16.0),
+                          TextFormField(
+                            obscureText: !passwordVisible,
+                            controller: passwordController,
+                            validator: (value) {
+                              if (value == null || value.trim().isEmpty) {
+                                return "error";
+                              }
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                                labelText: "Contraseña",
+                                border: const OutlineInputBorder(),
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    passwordVisible
+                                        ? Icons.visibility_off
+                                        : Icons.visibility,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      passwordVisible = !passwordVisible;
+                                    });
+                                  },
                                 )),
                           ),
-                      ],
-                    ),
-                  ),
-                  if (signInError != null)
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.warning_amber_rounded,
-                          color: Theme.of(context).colorScheme.error,
-                        ),
-                        const SizedBox(width: 8),
-                        Expanded(
-                          child: Text(
-                            signInError!,
-                            style: Theme.of(context)
-                                .textTheme
-                                .labelLarge
-                                ?.copyWith(
-                                  color: Theme.of(context).colorScheme.error,
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              _loginPressed();
+                            }
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text("Igresar"),
+                              if (isLogingIn)
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 8),
+                                  child: SizedBox.square(
+                                      dimension: 16,
+                                      child: CircularProgressIndicator(
+                                        color: Colors.white,
+                                      )),
                                 ),
+                            ],
                           ),
                         ),
-                      ],
-                    )
-                ],
+                      ),
+                      if (signInError != null)
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.warning_amber_rounded,
+                              color: Theme.of(context).colorScheme.error,
+                            ),
+                            const SizedBox(width: 8),
+                            Expanded(
+                              child: Text(
+                                signInError!,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .labelLarge
+                                    ?.copyWith(
+                                      color:
+                                          Theme.of(context).colorScheme.error,
+                                    ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      SizedBox(height: 24),
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
-        ),
-      ),
+        );
+      }),
     );
   }
 
