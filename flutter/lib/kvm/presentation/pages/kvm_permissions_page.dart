b77b8106eb0822b78cd19d2cd747e4c5bb910ef1
Teo Vogel <teovogel97@gmail.com>
2024-10-25 09:11:54 -0300

    Permissions page changes

--- flutter/lib/kvm/presentation/pages/kvm_permissions_page.dart
+++ flutter/lib/kvm/presentation/pages/kvm_permissions_page.dart
@@ -1,7 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hbb/common.dart';
+import 'package:flutter_hbb/kvm/domain/kvm_state_provider.dart';
+import 'package:flutter_hbb/kvm/kvm_routing_utils.dart';
+import 'package:flutter_hbb/kvm/presentation/widgets/kvm_app_bar.dart';
 import 'package:flutter_hbb/mobile/pages/server_page.dart';
 import 'package:flutter_hbb/models/server_model.dart';
+import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 
 class KVMPermissionsPage extends StatefulWidget {
@@ -12,6 +18,23 @@ class KVMPermissionsPage extends StatefulWidget {
 }
 
 class _KVMPermissionsPageState extends State<KVMPermissionsPage> {
+  Timer? _updateTimer;
+
+  @override
+  void initState() {
+    super.initState();
+    _updateTimer = periodic_immediate(const Duration(seconds: 3), () async {
+      await gFFI.serverModel.fetchID();
+    });
+    gFFI.serverModel.checkAndroidPermission();
+  }
+
+  @override
+  void dispose() {
+    _updateTimer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,26 +42,104 @@ class _KVMPermissionsPageState extends State<KVMPermissionsPage> {
           value: gFFI.serverModel,
           builder: (context, _) {
             return Consumer<ServerModel>(builder: (context, serverModel, _) {
-              return PaddingCard(
-                title: "Permisos",
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    PermissionRow("Screen Capture", serverModel.mediaOk,
-                        serverModel.toggleService),
-                    PermissionRow(
-                      "Input Control",
-                      serverModel.inputOk,
-                      serverModel.toggleInput,
+              return CustomScrollView(slivers: [
+                getKVMSliverAppBar(context),
+                SliverFillRemaining(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(13),
+                          ),
+                          margin: EdgeInsets.all(16),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  "Permisos",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headlineSmall
+                                      ?.copyWith(fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(height: 8),
+                                Text(
+                                  "Habilita TODOS los siguientes permisos para terminar el setup",
+                                ),
+                                SizedBox(height: 4),
+                                Divider(),
+                                SwitchListTile(
+                                  contentPadding: EdgeInsets.all(0),
+                                  title: Text("Input Control"),
+                                  value: serverModel.inputOk,
+                                  onChanged: (bool value) =>
+                                      serverModel.toggleInput(),
+                                ),
+                                SwitchListTile(
+                                  contentPadding: EdgeInsets.all(0),
+                                  title: Text("Transfer file"),
+                                  value: serverModel.fileOk,
+                                  onChanged: (bool value) =>
+                                      serverModel.toggleFile(),
+                                ),
+                                SwitchListTile(
+                                    contentPadding: EdgeInsets.all(0),
+                                    title: Text("Screen Capture"),
+                                    value: serverModel.mediaOk,
+                                    onChanged: serverModel.inputOk &&
+                                            serverModel.fileOk
+                                        ? (bool value) {
+                                            serverModel.toggleService();
+                                            serverModel.fetchID();
+                                          }
+                                        : null),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Visibility(
+                          visible: serverModel.mediaOk &&
+                              serverModel.inputOk &&
+                              serverModel.fileOk,
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 24,
+                                  horizontal: 16,
+                                ),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Icon(
+                                      Icons.check_circle_outline_rounded,
+                                      color: Colors.green,
+                                      size: 64,
+                                    ),
+                                    Text(
+                                      "Setup completo!",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .titleLarge,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              ServerInfo(),
+                              SizedBox(height: 24),
+                            ],
+                          ),
+                        )
+                      ],
                     ),
-                    PermissionRow(
-                      "Transfer file",
-                      serverModel.fileOk,
-                      serverModel.toggleFile,
-                    ),
-                  ],
+                  ),
                 ),
-              );
+              ]);
             });
           }),
     );
